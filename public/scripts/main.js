00c7b1a6ea7fbfb74c8c1a490aaf60240028625f
Pankaj Yadav <panjadotcom@icloud.com>
2020-09-04 22:47:39 +0530

    Add Table search option

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -10,3 +10,26 @@ $("div .alert").on('mouseover', (el) => {
     $("div .alert").remove();
   }, 500);
 });
+
+function filterTableContent(inputLabel, tableLabel) {
+    var input, filter, table, tr, td, cell, i, j;
+    input = document.getElementById(inputLabel);
+    filter = input.value.toUpperCase();
+    table = document.getElementById(tableLabel);
+    tr = table.getElementsByTagName("tr");
+    for (i = 1; i < tr.length; i++) {
+          // Hide the row initially.
+          tr[i].style.display = "none";
+      
+          td = tr[i].getElementsByTagName("td");
+          for (var j = 0; j < td.length; j++) {
+          cell = tr[i].getElementsByTagName("td")[j];
+          if (cell) {
+              if (cell.innerHTML.toUpperCase().indexOf(filter) > -1) {
+              tr[i].style.display = "";
+              break;
+              } 
+          }
+        }
+    }
+}
